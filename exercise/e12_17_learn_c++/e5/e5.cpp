690b66f9b0a561c68dddbc141ac52bcc5d40f7ae
Myess121 <mengdeyuan0537@163.com>
2025-12-18 10:30:45 +0800

    这个简单

---
+++ exercise/e12_17_learn_c++/e5/e5.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+unordered_map <string,int>a;
+int main()
+{
+    int n;
+    string s;
+    scanf("%d",&n);
+    for(int i = 0;i<n;i++)
+    {
+        cin>>s;
+        if(a[s]){}
+    }
+    cout<<a.size()<<endl;
+
+    return 0;
+}
