997db3d6a4584f8ea253fd1e673c7b24bfce9d86
Myess121 <mengdeyuan0537@163.com>
2025-12-29 16:16:52 +0800

    68【链表】排序

---
+++ homework/Week16/h1/h1.c
@@ -0,0 +1,59 @@
+#include "stdio.h"  
+#include "stdlib.h"  
+struct node  
+{   int data;  
+    struct node * next;  
+} ;  
+  
+typedef struct node NODE;  
+typedef struct node * PNODE;  
+void outlist( PNODE );  
+void sortlist( PNODE, int );  
+  
+int main ( )  
+{   int num=1;  
+    PNODE head;  
+  
+    head = (PNODE)malloc( sizeof(NODE) );  
+    head->next = NULL;  
+    head->data = -1;  
+  
+    while ( num!=0 )  
+    {   scanf("%d", &num);  
+        if ( num!=0 )  
+            sortlist( head, num);  
+    }  
+    outlist( head );  
+    return 0;  
+}  
+  
+void outlist( PNODE head )  
+{   PNODE p;  
+    p = head->next;  
+    while ( p != NULL )  
+    {   printf("%d\n", p->data);  
+        p = p->next;  
+    }  
+}  
+  
+
+void sortlist( PNODE h, int num ) 
+{   
+    PNODE p,p1 = h; 
+    p = (PNODE)malloc( sizeof(NODE) ); 
+    p->data = num;
+    while(h ->next != NULL)
+    {
+        h = h->next;
+        if(h->data > num){
+            break;
+        }
+        if(h->data == num){
+            return ;
+        }
+        p1 = h;
+    }
+    p->next = p1->next;
+    p1->next = p;
+
+} 
