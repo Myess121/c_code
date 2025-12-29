8623819131f4900bc8497b4aa640ba60ca8313d9
Myess121 <mengdeyuan0537@163.com>
2025-12-29 17:02:26 +0800

    vscode不知道运行的目录，所以文件题用dev做

---
+++ homework/Week16/h2/h2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    char a[50],b[50];
+    char x,y;
+    int flag;
+    scanf("%s %s",a,b);
+    FILE * fp = fopen(a,"r");
+    FILE *f2 = fopen(b,"r");
+    while((x = fgetc(f2))!=EOF)
+    {
+        rewind(fp);
+        flag = 0;
+        while((y = fgetc(fp))!=EOF)
+        {
+            if(y == x){
+                y = fgetc(fp);
+                if(y == ','){
+                    y = fgetc(fp);
+                    printf("%c",y);
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        if(flag == 0){
+        	printf("%c",x);
+		}
+    }
+
+    return 0;
+}
